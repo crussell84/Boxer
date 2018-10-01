056f977f3524bef284a367f9c2def6507c510caa
rickknowlton <richard.d.knowlton@gmail.com>
2018-10-01 19:52:26 -0400

    Dashboard/Products Front End

---
+++ assets/js/main.js
@@ -0,0 +1,132 @@
+// Materialize Button Collapse Function
+$(document).ready(function() {
+
+(function () {
+    $(".button-collapse").sideNav();
+    $('.collapsible').collapsible();
+    $('select').material_select();
+    var bootTimeout = window.setTimeout(function setBootTimeout() {
+
+    })();
+});
+
+// Chart.js Function
+
+var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+'November', 'December'
+];
+var color = Chart.helpers.color;
+var barChartData = {
+labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+datasets: [{
+    label: 'Purchases',
+    backgroundColor: color(window.chartColors.grey).alpha(0.6).rgbString(),
+    borderColor: window.chartColors.grey,
+    borderWidth: 1,
+    data: [
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor()
+    ]
+}, {
+    label: 'Sales',
+    backgroundColor: color(window.chartColors.blue).alpha(0.6).rgbString(),
+    borderColor: window.chartColors.blue,
+    borderWidth: 1,
+    data: [
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor(),
+        randomScalingFactor()
+    ]
+}]
+
+};
+
+window.onload = function () {
+var ctx = document.getElementById('canvas').getContext('2d');
+window.myBar = new Chart(ctx, {
+    type: 'bar',
+    data: barChartData,
+    options: {
+        responsive: true,
+        legend: {
+            position: 'top',
+        },
+        title: {
+            display: true,
+            text: 'Purchases vs Sales'
+        }
+    }
+});
+
+};
+
+document.getElementById('randomizeData').addEventListener('click', function () {
+var zero = Math.random() < 0.2 ? true : false;
+barChartData.datasets.forEach(function (dataset) {
+    dataset.data = dataset.data.map(function () {
+        return zero ? 0.0 : randomScalingFactor();
+    });
+
+});
+window.myBar.update();
+});
+
+var colorNames = Object.keys(window.chartColors);
+document.getElementById('addDataset').addEventListener('click', function () {
+var colorName = colorNames[barChartData.datasets.length % colorNames.length];
+var dsColor = window.chartColors[colorName];
+var newDataset = {
+    label: 'Dataset ' + barChartData.datasets.length,
+    backgroundColor: color(dsColor).alpha(0.5).rgbString(),
+    borderColor: dsColor,
+    borderWidth: 1,
+    data: []
+};
+
+for (var index = 0; index < barChartData.labels.length; ++index) {
+    newDataset.data.push(randomScalingFactor());
+}
+
+barChartData.datasets.push(newDataset);
+window.myBar.update();
+});
+
+document.getElementById('addData').addEventListener('click', function () {
+if (barChartData.datasets.length > 0) {
+    var month = MONTHS[barChartData.labels.length % MONTHS.length];
+    barChartData.labels.push(month);
+
+    for (var index = 0; index < barChartData.datasets.length; ++index) {
+        // window.myBar.addData(randomScalingFactor(), index);
+        barChartData.datasets[index].data.push(randomScalingFactor());
+    }
+
+    window.myBar.update();
+}
+});
+
+document.getElementById('removeDataset').addEventListener('click', function () {
+barChartData.datasets.splice(0, 1);
+window.myBar.update();
+});
+
+document.getElementById('removeData').addEventListener('click', function () {
+barChartData.labels.splice(-1, 1); // remove the label first
+
+barChartData.datasets.forEach(function (dataset) {
+    dataset.data.pop();
+});
+
+window.myBar.update();
+});
+
+});
