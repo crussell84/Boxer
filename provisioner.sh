9c7f41f22a2887a5edd9c31e12e06fded4a0a31c
Chris Russell <christine.russell10@gmail.com>
2018-10-03 19:43:40 -0400

    update mysql default pw

--- provisioner.sh
+++ provisioner.sh
@@ -21,7 +21,7 @@ sudo apt-get install libcap2-bin
 sudo setcap cap_net_bind_service=+ep `readlink -f \`which node\``
 
 # #install mySql
-# debconf-set-selections <<< 'mysql-server mysql-server/root_password password knitgameplay'
-# debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password knitgameplay'
+# debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+# debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 
 # apt-get install -y mysql-server
